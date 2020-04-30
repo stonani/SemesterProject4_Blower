afa8e51f7b5665dacc6fa2367bf6243ead8d42d2
Stefine <stefine.varde@gmail.com>
2020-04-30 15:28:40 +0200

    Initial commit

---
+++ Projekt.ino
@@ -0,0 +1,37 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Projekt.ino
+    Created:	23-03-2020 13:53:29
+    Author:     DESKTOP-F4TABSH\ander
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+
+const int StartButtonIn = A3;
+const int StopButtonIn = A5;
+const int 
+
+//
+void setup()
+{
+    
+}
+
+//
+// Add the main program code into the continuous loop() function
+void loop()
+{
+  
+}
