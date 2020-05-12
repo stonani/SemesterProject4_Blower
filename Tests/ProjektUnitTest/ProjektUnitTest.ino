0a4989ce9a9587017bb7ba43c99dd255b7647cc6
Nicolaj Kvejborg Hansen <au461460@uni.au.dk>
2020-05-12 17:41:24 +0200

    tilføjelse

--- Tests/ProjektUnitTest/ProjektUnitTest.ino
+++ Tests/ProjektUnitTest/ProjektUnitTest.ino
@@ -10,14 +10,43 @@ FakeIndicator myIndicator;
 
 SystemControl uut;
 
-test(StartButtonNotPressed_StateFalse_CallStopBlowerTurnOff) {
-  uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
-  
-  assertEqual(1, 1);
+
+
+test(StartButtonNotPressed_StateFalse_OneCallRecieved) {
+ 
+  assertEqual(myControlPanel.startCounter,1); 
+ 
+}
+
+test(StartButtonNotPressed_StateFalse_StopBlowerTurnOffIdicator) {
+ 
+  assertEqual(myBlower.stopCounter,1); 
+  assertEqual(myIndicator.OFFcounter,1);
+ 
+}
+
+test(StopButtonNotPressed_StateFalse_OneCallRecieved) {
+ 
+  assertEqual(myControlPanel.stopCounter,1); 
+}
+
+test(HumiditySensor1_FloatZero_OneCallRecieved) {
+ 
+  assertEqual(myHumiditySensor.sensor1Counter,1); 
+ 
+}
+
+test(HumiditySensor2_FloatZero_OneCallRecieved) {
+ 
+  assertEqual(myHumiditySensor.sensor2Counter,1); 
+ 
 }
 
 
+
 void setup() {
+  uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
+  uut.Loop();
   delay(1000); // wait for stability on some boards to prevent garbage Serial
   Serial.begin(9600); // ESP8266 default of 74880 not supported on Linux
   while(!Serial); // for the Arduino Leonardo/Micro only
@@ -25,4 +54,5 @@ void setup() {
 
 void loop() {
     aunit::TestRunner::run();
+    delay(50);
 }
