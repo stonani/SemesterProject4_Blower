ffacecca8c5e4551a7a2cc697ef2cea4feac7c70
Nicolaj Kvejborg Hansen <au461460@uni.au.dk>
2020-05-13 00:31:04 +0200

    problemer med merge

--- Tests/ProjektUnitTest/ProjektUnitTest.ino
+++ Tests/ProjektUnitTest/ProjektUnitTest.ino
@@ -8,23 +8,19 @@ FakeBlower myBlower;
 FakeControlPanel myControlPanel;
 FakeHumiditySensor myHumiditySensor;
 FakeIndicator myIndicator;
-      
-  SystemControl uut;
-
+SystemControl uut;
+void resetCounters();
 int N=0;
 
-<<<<<<< HEAD
+
 
 class StartButtonNotPressed_stateFalse_IsStartButtonPressedOneCall: public TestOnce {
   protected:
     void setup() override {
-      void resetCounters();                
       uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
       uut.Loop();
     }
-    
- 
-                         
+                             
 };
 
 testF(StartButtonNotPressed_stateFalse_IsStartButtonPressedOneCall,simple1) {
@@ -35,7 +31,6 @@ testF(StartButtonNotPressed_stateFalse_IsStartButtonPressedOneCall,simple1) {
 class StartButtonNotPressed_StateFalse_StopBlowerTurnOffIndicator: public TestOnce {
   protected:
     void setup() override {
-      void resetCounters();                
       uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
       uut.Loop();
     }   
@@ -56,12 +51,11 @@ testF(StartButtonNotPressed_StateFalse_StopBlowerTurnOffIndicator,t1) {
 class StopButtonNotPressed_StateFalse_OneCallRecieved: public TestOnce {
   protected:
     void setup() override {
-      void resetCounters();                
       uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
       uut.Loop();
     }
     
-     void resetCounters(); 
+      
     
    
 };
@@ -75,7 +69,6 @@ class StartbuttonPressed_StateTrue_StartBlowerTurnOnIndicator: public TestOnce {
   protected:
     void setup() override {
 
-      void resetCounters();
       uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
       myControlPanel.returnValueStart = true;
       uut.Loop();
@@ -99,7 +92,6 @@ testF(StartbuttonPressed_StateTrue_StartBlowerTurnOnIndicator, simple2){
 class HumiditySensor1_FloatZero_OneCallRecieved: public TestOnce {
   protected:
     void setup() override {
-      void resetCounters();
      uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
       uut.Loop();
     }
@@ -119,7 +111,6 @@ testF(HumiditySensor1_FloatZero_OneCallRecieved,simple4) {
 class HumiditySensor2_FloatZero_OneCallRecieved: public TestOnce {
   protected:
     void setup() override {
-     .resetCounters();
      uut.SetUp(&myBlower,&myControlPanel,&myHumiditySensor,&myIndicator);
       uut.Loop();
     }
@@ -130,13 +121,30 @@ class HumiditySensor2_FloatZero_OneCallRecieved: public TestOnce {
 testF(HumiditySensor2_FloatZero_OneCallRecieved,simple5) {
  
   assertEqual(myHumiditySensor.sensor2Counter,1); 
- 
+
 }
 
 
 
+
+
+
+void setup() {
+  
+  delay(1000); // wait for stability on some boards to prevent garbage Serial
+  Serial.begin(9600); // ESP8266 default of 74880 not supported on Linux
+  while(!Serial); // for the Arduino Leonardo/Micro only
+}
+
+void loop() {
+   
+    aunit::TestRunner::run();
+    delay(50);
+}
+
 void resetCounters()
 {
+  N=5;
       myBlower.blowerCounter=0;
       myBlower.startCounter=0;
       myBlower.stopCounter=0;
@@ -152,17 +160,4 @@ void resetCounters()
       myHumiditySensor.returnValueSensor2=0;
       myIndicator.ONcounter=0;
       myIndicator.OFFcounter=0;
-}
-
-void setup() {
-  
-  delay(1000); // wait for stability on some boards to prevent garbage Serial
-  Serial.begin(9600); // ESP8266 default of 74880 not supported on Linux
-  while(!Serial); // for the Arduino Leonardo/Micro only
-}
-
-void loop() {
-   
-    aunit::TestRunner::run();
-    delay(50);
 }
